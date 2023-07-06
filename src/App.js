d2eed3b10e693a7b2b80030f636290a21c5d0a76
tronprogram <bassgamer1234@outlook.es>
2023-07-06 11:56:53 -0500

    new file:   public/favicon.svg 	modified:   public/index.html 	new file:   public/tron.woff 	new file:   public/tron.woff2 	modified:   src/App.js 	modified:   src/components/Banner.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import './fonts.css';
 
 const App = () => {
   return (
+    
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      
       <Header />
       <Banner />
       <Nav />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,14 +7,14 @@ import { fadeIn } from '../variants.js';
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section className="min-h-[65vh] lg:min-h-[78vh] flex items-center" id="home">
       <div className="container mx-auto">
-        <div>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
-              JOHAN <span>PORTILLA AGUIRRE</span>
-            </h1>
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font semibold uppercase leading-[1]'>
+            <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+              JOHAN <span>PORTILLA</span>
+            </motion.h1>
+            <motion.div variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}}  className='mb-6 text-[36px] lg:text-[60px] font-secondary font semibold uppercase leading-[1]'>
               <span className='text-white mr-4'>Soy un </span>
               <TypeAnimation sequence={[
                 'Desarrollador',
@@ -28,11 +28,11 @@ const Banner = () => {
               className='text-accent'
               wrapper='span'
               repeat={Infinity}/>
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0 '>
+            </motion.div>
+            <motion.p variants={fadeIn('up', 0.5)} initial="hidden" whileInView={'show'} viewport={{once: false, amount:0.7}}  className='mb-8 max-w-lg mx-auto lg:mx-0 '>
               Soy un desarrollador que vive en Piedecuesta, Santander, Colombia. Me gusta investigar y aprender cosas que nunca he pensado hacer.
-            </p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.p>
+            <div className='flex max-w-max gap-x-12 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-lg'>Contáctame</button>
               <a href='#' className='text-gradient btn-link'>Mi Portafolio</a>
             </div>
@@ -48,7 +48,7 @@ const Banner = () => {
               </a>
             </div>
           </div>
-          <div>
+          <div className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[428px]'>
             <img src={avatarImage} alt="Avatar" />
           </div>
         </div>
