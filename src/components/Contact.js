cf36d95b07fa0c80334f4400aae0bd389bc8f939
tronprogram <bassgamer1234@outlook.es>
2023-07-08 19:56:13 -0500

    new file:   src/assets/img4.png 	new file:   src/assets/img5.png 	new file:   src/assets/img6.png 	modified:   src/assets/portfolio-img1.png 	modified:   src/assets/portfolio-img2.png 	modified:   src/assets/portfolio-img3.png 	modified:   src/components/Contact.js 	modified:   src/components/Work.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import {motion} from 'framer-motion'
+import {fadeIn} from '../variants'
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+  return <div className='py-16 lg:section ' id='contact'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row'>
+        <motion.div variants={fadeIn('right', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once:false, amount:0.3}} className='flex-1 flex justify-start items-center'>
+          <div>
+          <h4 className='text-x1 uppercase text-accent font-medium mb-2 tracking-wide'>Contáctame</h4>
+          <h2 className='text-[45px] uppercase lg:text-[90px] leading-none mb-12'>Trabajemos <br/>juntos!</h2>
+          </div>
+        </motion.div>
+        <motion.form variants={fadeIn('left', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once:false, amount:0.3}} className='flex-1 border rounded flex flex-col gap-y-6 pb-24 p-6 items-start'>
+          <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type='text' placeholder='Tu nombre'></input>
+          <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type='text' placeholder='Tu e-mail'></input>
+          <textarea className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12' placeholder='Tu mensaje'></textarea>
+          <button className='btn btn-lg'>Enviar Mensaje</button>
+        </motion.form>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Contact;
--- src/components/Work.js
+++ src/components/Work.js
@@ -4,31 +4,93 @@ import {fadeIn} from '../variants'
 import Img1 from '../assets/portfolio-img1.png'
 import Img2 from '../assets/portfolio-img2.png'
 import Img3 from '../assets/portfolio-img3.png'
+import Img4 from '../assets/img4.png'
+import Img5 from '../assets/img5.png'
+import Img6 from '../assets/img6.png'
 const Work = () => {
   return <div className='section' id='work'>
     <div className='container mx-auto'>
-      <div className='flex flex-col lg-flex-row gap-x-10'>
-        <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+      <div className='flex flex-col lg:flex-row gap-x-10'>
+        <motion.div variants={fadeIn('right', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once:false, amount:0.3}} className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
           {/* text */}
           <div>
             <h2 className='h2 leading-tight text-accent'>Mis trabajos más <br></br>recientes</h2>
-            <p>Acá puedes ver los diferentes proyectos y trabajos que he realizado.</p>
-            <button className='btn btn-sm'>Ver todos los proyectos</button>
+            <p>Acá puedes ver los proyectos más recientes que he hecho.</p>
+            <button className='btn btn-sm mt-10 mb-24'>Ver todos los proyectos</button>
           </div>
           {/* image */}
-          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+          <a href='https://crud-seven-rho.vercel.app'><div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
             {/* overlay */}
             <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
             {/* img */}
             <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="heya1"></img>
             {/* pretitle */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>PLACEHOL1</span></div>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>CRUD en NextJS</span></div>
             {/*title*/}
             <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-              <span className='text-3xl text-white'>projtitle</span></div>
+              <span className='text-3xl text-white'>App para crear tareas en NextJS</span></div>
+          </div></a>
+          <div><h2 className='h2 leading-tight text-accent'>Mis trabajos <br></br>antiguos</h2>
+            <p>Acá puedes ver los diferentes proyectos y trabajos que he realizado.</p></div>
+          <a href='https://crud-seven-rho.vercel.app'><div className='group relative overflow-hidden border-2 h-40 border-white/50 rounded-xl'>
+            {/* ay chatgpt check over 'ere */}
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            {/* img */}
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="heya1"></img>
+            {/* pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>h</span></div>
+            {/*title*/}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>App para crear tareas en NextJS</span></div>
+          </div></a>
+        </motion.div>
+        <motion.div variants={fadeIn('left', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once:false, amount:0.3}}
+            className='flex-1 flex flex-col gap-y-10'>
+          {/* image */}
+          <a href='https://teamtbd-site.vercel.app'><div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            {/* overlay */}
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            {/* img */}
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt="heya2"></img>
+            {/* pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>SITIO TEAMTBD</span></div>
+            {/*title*/}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Sitio creado para mi empresa TeamTBD</span></div>
+          </div></a>
+          {/* image  start copying over again on 'ere*/}
+          <a href='https://teal-scone-eea30c.netlify.app'><div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            {/* overlay */}
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            {/* img */}
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt="heya3"></img>
+            {/* pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>FRASES DE BREAKING BAD</span></div>
+            {/*title*/}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Sitio que genera frases de Breaking Bad</span></div>
           </div>
-        </div>
-        <div>2</div>
+          </a>
+          <a href='https://incredible-bubblegum-6b9318.netlify.app' className=''><div className='group relative overflow-hidden border-2 border-white/50 rounded-xl h-40 mt-52'>
+            {/* overlay */}
+            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            {/* img */}
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt="heya3"></img>
+            {/* pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>SITIO DE PRESUPUESTO</span></div>
+            {/*title*/}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>App para manejo de presupuesto</span></div>
+          </div>
+          </a>
+
+        </motion.div>
       </div>
     </div>
   </div>;
